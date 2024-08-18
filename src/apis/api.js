7e1cf1f21d3b8190fc2073a3d10408067e7906cf
Sayed Ishahath Ahmed <68497092+sayedishahath@users.noreply.github.com>
2024-08-18 19:25:58 +0530

    Update api.js

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,9 +2,12 @@
 export const API_COMMON_PATH = 'https://task-cart-backend.onrender.com/api/'
 
 export const LOGIN = API_COMMON_PATH+'users/login'
+export const ACCOUNT = API_COMMON_PATH+'users/account'
 export const REGISTER = API_COMMON_PATH+'users/register'
 export const CREATE_CART = API_COMMON_PATH+'user/cart/'
 export const GET_MY_CART = API_COMMON_PATH+'user/cart/'
 export const DELETE_CART_ITEM = API_COMMON_PATH+'user/cart/'
 export const INC_QTY = API_COMMON_PATH+'user/cart/inc/'
 export const DEC_QTY = API_COMMON_PATH+'user/cart/dec/'
+
+
